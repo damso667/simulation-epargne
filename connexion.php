b02636f45097b5635543418f556be2c74f25d06f
Adrien Christien <njassineadrien@gmail.com>
2026-02-13 03:01:28 +0100

    first commit

--- connexion.php
+++ connexion.php
@@ -1,25 +1,27 @@
 <?php
-class connecte{
+class connecte
+{
     private $serveur;
     private $login;
     private $dbname;
     private $pass;
 
-    public function  __construct(){
-        $this->serveur = "localhost";
-        $this->login  = "root";
-        $this->dbname = "gestion_qalf";
-        $this->pass = "";
+    public function  __construct()
+    {
+        $this->serveur = "sql309.infinityfree.com";
+        $this->login  = "if0_41145457";
+        $this->dbname = "if0_41145457_simulationbd";
+        $this->pass = "t3SUbChEaWs";
     }
 
-    public function conexion(){
-        try{
-            $connection = new PDO("mysql:host=$this->serveur;dbname=$this->dbname;charset=utf8;",$this->login,$this->pass);
-            $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        }catch(exception $e){
-            echo "erreur : " .$e->getmessage();
+    public function conexion()
+    {
+        try {
+            $connection = new PDO("mysql:host=$this->serveur;dbname=$this->dbname;charset=utf8;", $this->login, $this->pass);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (exception $e) {
+            echo "erreur : " . $e->getmessage();
         }
-       return $connection;
+        return $connection;
     }
 }
-
